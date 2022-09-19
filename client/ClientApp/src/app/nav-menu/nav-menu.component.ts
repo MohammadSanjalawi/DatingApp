d42dddb47b88332dd17a17ace0bd6483a5093ec8
mohammadsanjalawi <m7md-sanjalawi@hotmail.com>
2022-09-19 11:30:08 +0300

    .

--- client/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ client/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.css']
 })
-export class NavMenuComponent {
+export class NavMenuComponent implements OnInit {
+  model: any = {}
+  LoggedIn: boolean;
+
+
+  constructor(private accountService: AccountService) {}
+
+  ngOnInit(): void {
+      throw new Error('Method not implemented.');
+  }
   isExpanded = false;
 
   collapse() {
@@ -14,5 +24,20 @@ export class NavMenuComponent {
 
   toggle() {
     this.isExpanded = !this.isExpanded;
+  }
+
+
+  login() {
+    this.accountService.login(this.model).subscribe(response => {
+      console.log(response)
+      this.LoggedIn = true;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+
+  logout() {
+    this.LoggedIn = false;
   }
 }
